1624b6d45fa858531f8949455547fc43bab9e675
josipcodes <>
2023-07-20 09:28:01 +0000

    Add calculated surplus data to the worksheet

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
+
 def get_sales_data():
     """
     Get sales figures from the user.
@@ -36,6 +37,7 @@ def get_sales_data():
 
     return sales_data
 
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -56,6 +58,7 @@ def validate_data(values):
     
     return True
 
+
 def update_sales_worksheet(data):
     """
     Update worksheet with data in a new row.
@@ -64,6 +67,17 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet update successfully!\n")
+
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet with data in a new row.
+    """
+    print("Updating surplus worksheet.....\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet update successfully!\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -84,6 +98,7 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+
 def main():
     """
     Run all program functions
@@ -92,6 +107,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches data automation")
